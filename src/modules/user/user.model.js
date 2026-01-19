0fdb3d531ef0d88c40c3944c2a94627d54f5a766
emonmahdi <developeremon1997@gmail.com>
2026-01-19 12:37:09 +0600

    auth folder structure

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        role: {type: String, enum:['user', 'admin'], default:'user'},
+        status: {type: String, enum:['active', 'blocked'], default:'active'}
+    },
+    {
+        timestamps: true
+    }
+)
+
+// password hashed
+userSchema.pre('save', async function(next){
+    if(!this.isModified(password)) return next();
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+})
+
+export const User = mongoose.model('User', userSchema)
